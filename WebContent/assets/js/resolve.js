bb97948e3b387c43536ba8a98369b361529e7468
Eli27 <pawelhuszcza1986@gmail.com>
2016-03-22 00:18:53 +0000

    first commit

---
+++ WebContent/assets/js/resolve.js
@@ -0,0 +1,28 @@
+
+requirejs.config({
+	baseUrl: "assets/js",
+	paths: {
+		jquery: [
+		   '//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min',
+		   'lib/jquery'
+		],
+		underscore: [
+		   '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
+		   'lib/underscore'
+		],
+		backbone: [
+		   '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min',
+		   'lib/backbone'
+		],
+		game: 'localfiles/game',
+		data: 'localfiles/data',
+		updater: 'localfiles/updater',
+		handlers: 'localfiles/handlers',
+		states_list: 'localfiles/states_list',
+		animation_balancer: 'localfiles/animation_balancer'
+	}
+});
+
+require(['localfiles/game'], function(game){
+	game.play();
+});
